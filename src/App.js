706409b0fc1fa0c261aa9a8f887bf1e532d47d18
tobiforsdyke <tobiforsdyke@gmail.com>
2020-10-14 20:42:53 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from "react";
+import { Viewer } from "photo-sphere-viewer";
 import './App.css';
+import "../node_modules/photo-sphere-viewer/dist/photo-sphere-viewer.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   const sphereElementRef = React.createRef();
+
+//   useEffect(() => {
+//     const shperePlayerInstance = new Viewer({
+//       container: sphereElementRef.current,
+//       panorama:
+//         "https://upload.wikimedia.org/wikipedia/commons/f/f0/Colonia_Ulpia_Traiana_-_Rekonstruktion_r%C3%B6mischer_Schiffe-0010560.jpg",
+//     });
+
+//     // unmount component instructions
+//     return () => {
+//       shperePlayerInstance.destroy();
+//     };
+//   }, []); // will only be called when the src prop gets updated
+
+//   return <div ref={sphereElementRef} />;
+// };
+
+// export default App;
+
+
+export default function App(props) {
+  const sphereElementRef = React.createRef();
+  const { src } = props;
+
+  useEffect(() => {
+    const shperePlayerInstance = new Viewer({
+      container: sphereElementRef.current,
+      panorama:
+        "https://upload.wikimedia.org/wikipedia/commons/f/f0/Colonia_Ulpia_Traiana_-_Rekonstruktion_r%C3%B6mischer_Schiffe-0010560.jpg",
+      navbar: [
+        'autorotate',
+        'zoom',
+        'fullscreen'
+      ]
+    });
+    // unmount component instructions
+    return () => {
+      shperePlayerInstance.destroy();
+    };
+  }, [src]); // will only be called when the src prop gets updated
+
+  return <div ref={sphereElementRef} />;
 }
-
-export default App;
