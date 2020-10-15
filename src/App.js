09c878df2b035462c81d513a657ed403e9cd22de
tobiforsdyke <tobiforsdyke@gmail.com>
2020-10-15 13:10:38 +0100

    update to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Viewer } from "photo-sphere-viewer";
 import './App.css';
-import "../node_modules/photo-sphere-viewer/dist/photo-sphere-viewer.css";
+// import "../node_modules/photo-sphere-viewer/dist/photo-sphere-viewer.css";
 
 function App() {
   const sphereElementRef = React.createRef();
@@ -11,13 +11,18 @@ function App() {
       container: sphereElementRef.current,
       panorama:
         "https://upload.wikimedia.org/wikipedia/commons/f/f0/Colonia_Ulpia_Traiana_-_Rekonstruktion_r%C3%B6mischer_Schiffe-0010560.jpg",
+      navbar: [
+        'autorotate',
+        'zoom',
+        'fullscreen'
+      ]
     });
 
     // unmount component instructions
     return () => {
       shperePlayerInstance.destroy();
     };
-  }, []); // will only be called when the src prop gets updated
+  }, [sphereElementRef]); // will only be called when the src prop gets updated
 
   return <div ref={sphereElementRef} />;
 };
